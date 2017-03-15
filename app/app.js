bff5d7b079512877f2e0c0a7b2c5c32669e411b5
Thierry Sans <thierry.sans@gmail.com>
2017-03-15 10:18:19 -0400

    exit

--- app/app.js
+++ app/app.js
@@ -14,3 +14,8 @@ var http = require("http");
 http.createServer(app).listen(80, function(){
     console.log('HTTP on port 80');
 });
+
+process.on('SIGTERM', function() {
+  console.log('\ncaught SIGTERM, stopping gracefully');
+  process.exit(1);
+});
