47e3b2c72a0c0d2a290536b70b9587d5240bc1c0
Thierry Sans <thierry.sans@gmail.com>
2017-03-21 11:44:25 -0400

    ssl v2

--- app/app.js
+++ app/app.js
@@ -10,28 +10,7 @@ app.get('/', function (req, res, next) {
     res.end("Now works with HTTPS!");
 });
 
-// var http = require("http");
-// http.createServer(app).listen(3000, function(){
-//     console.log('HTTP on port 3000');
-// });
-
-/* **********
-*** HTTPS ***
-************* */
-
-// Generate a self-signed certificate
-// openssl req -x509 -nodes -newkey rsa:4096 -keyout server.key -out server.crt
-// Read the certificate 
-// openssl x509 -in server.crt -text -noout
-
-var fs = require('fs');
-var https = require('https');
-var privateKey = fs.readFileSync( 'server.key' );
-var certificate = fs.readFileSync( 'server.crt' );
-var config = {
-        key: privateKey,
-        cert: certificate
-};
-https.createServer(config, app).listen(3000, function () {
-    console.log('HTTPS on port 3000');
+var http = require("http");
+http.createServer(app).listen(3000, function(){
+    console.log('HTTP on port 3000');
 });
