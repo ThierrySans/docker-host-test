c4294f1742192935d3eab4aab658236689ccce01
Thierry Sans <thierry.sans@gmail.com>
2017-03-24 19:09:40 -0400

    file test

--- app/app.js
+++ app/app.js
@@ -1,13 +1,38 @@
+var fs = require('fs');
 var express = require('express')
 var app = express();
+var async = require('async');
+
+// var MongoClient = require('mongodb').MongoClient;
+// MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+//     if (err) console.log(err);
+//     else console.log("Mongo database connected");
+// });
 
 app.use(function (req, res, next){
-    console.log("HTTP request", req.method, req.url, req.body);
+    console.log("HTTP request", req.method, req.url);
     next();
 });
 
+var testFile = function(callback){
+    fs.writeFile("/home/uploads/myFile.txt", "This text is from a local file!", function(err) {
+        if (err) return callback(err.message, null);
+        fs.readFile("/home/uploads/myFile.txt", function(err, res){
+            if (err) return callback(err.message, null);
+            return callback(null, res);
+        });
+    }); 
+};
+
 app.get('/', function (req, res, next) {
-    res.end("Now works with HTTPS!");
+    var tasks = [];
+    tasks.push(testFile);
+    async.series(tasks, function(err, results){
+        console.log(err);
+        if (err) return res.status(500).end(err);
+        results.push("And it works with HTTPS!");
+        return res.end(results.join("\n"));
+    });
 });
 
 var http = require("http");
